3125240bec835ecbc6ea5cab4a9d924be6dc63b7
gonzafer95 <162608632+gonzafer95@users.noreply.github.com>
2024-03-17 04:41:26 -0300

    Update server.py

--- server.py
+++ server.py
@@ -15,4 +15,4 @@ add_routes(app, chain)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=env.PORT)
+    uvicorn.run(app, host="0.0.0.0", port=process.env.PORT)
