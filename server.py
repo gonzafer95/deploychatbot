d107a436af345ae54d6f583275a90a83c1047628
gonzafer95 <162608632+gonzafer95@users.noreply.github.com>
2024-03-17 04:50:25 -0300

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ app = FastAPI(
 
 add_routes(app, chain)
 
-if __name__ == "__main__":
-    import uvicorn
+#if __name__ == "__main__":
+#    import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#   uvicorn.run(app, host="0.0.0.0", port=8000)
