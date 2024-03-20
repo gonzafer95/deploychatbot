c577272d9c5ef43d4819ce3e9d5b1ea76d81a93a
gonzafer95 <162608632+gonzafer95@users.noreply.github.com>
2024-03-20 13:08:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_html(url):
   chrome_options = Options()
   chrome_options.add_argument("--headless")
   chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--lang=es")
+  chrome_options.add_argument("--lang=es")
   #chrome_options.add_argument("--disable-dev-shm-usage")
 
   #svc = webdriver.ChromeService(executable_path=binary_path)
